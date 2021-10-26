4825a2b38a19d468f59b9937aaa3b3f66d203cdb
yuletsypabon <yuletsyp.pabonf@unac.edu.co>
2021-10-26 11:01:37 -0500

    rutas

--- spotify-a/src/app/app.module.ts
+++ spotify-a/src/app/app.module.ts
@@ -5,10 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [ //TODO:DECLARACIONES, COMPONENTS,DIRECTIVAS, PIPES
+    AppComponent,
   ],
-  imports: [
+  imports: [ //TODO: SOLO SE IMPORTAN OTROS MODULOS
     BrowserModule,
     AppRoutingModule
   ],
---
+++ spotify-a/src/app/shared/shared.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SideBarComponent } from './components/side-bar/side-bar.component';
+import { HeaderUserComponent } from './components/header-user/header-user.component';
+import { MediaPlayerComponent } from './components/media-player/media-player.component';
+
+
+
+@NgModule({
+  declarations: [
+    SideBarComponent,
+    HeaderUserComponent,
+    MediaPlayerComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    SideBarComponent
+  ]
+})
+export class SharedModule { }
